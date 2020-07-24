0815f0adff7cd9ea905cf21e1be849dc6de6a940
lukaszdyszy <mruhol123@gmail.com>
2020-07-24 12:39:00 +0200

    Updates

---
+++ precache-manifest.5a9a12bfb084e230672ab96108375e9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362dba76920214c0c96d415d75abf847",
+    "url": "/currency-calculator-react/index.html"
+  },
+  {
+    "revision": "c7957147f90d4ee99a46",
+    "url": "/currency-calculator-react/static/css/main.fed2e18f.chunk.css"
+  },
+  {
+    "revision": "e0354abb3a8f380822b2",
+    "url": "/currency-calculator-react/static/js/2.68d4bffc.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/currency-calculator-react/static/js/2.68d4bffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7957147f90d4ee99a46",
+    "url": "/currency-calculator-react/static/js/main.55c59d11.chunk.js"
+  },
+  {
+    "revision": "38eb70fa68fc2579fd49",
+    "url": "/currency-calculator-react/static/js/runtime-main.18680e62.js"
+  }
+]);
